8152e0713d7cc969528c3c39054781032564bd93
Deivid <deividvelasquez122@gmail.com>
2024-02-10 13:32:33 -0500

    Trabajo en clase

--- pruebaInicial.py
+++ pruebaInicial.py
@@ -3,8 +3,27 @@ def pruebaInicial():
     
     with open('Inscritos.json','r') as archivo:
         datos = json.load(archivo)
-    inscritos = datos['datos']['Inscripcion']
-    id_camper = int(input('Ingresa el id del camper: '))
-    for inscrito in inscritos:
-        if inscrito['id'] == id_camper:
-            
+    with open('aprobados.json','r') as archivo:
+        datos2 = json.load(archivo)
+    while len(datos2['aprobados']['P1']) <= 33:
+        inscritos = datos['datos']['Inscripcion']
+        id_camper = int(input('Ingresa el id del camper: '))
+        aprobados = {}
+        estado = 'Aprobado'
+        for inscrito in inscritos:
+            if inscrito['id'] == id_camper:
+                notaPractica = int(input('Ingrese la nota de la prueba practica: '))
+                notaTeorica = int(input('Ingrese la nota de la prueba teorica: '))
+                notafinal = (notaPractica + notaTeorica)/2
+                if notafinal >= 60:
+                    aprobados['identidad'] = inscrito['identidad']
+                    aprobados['nombre'] = inscrito['nombre']
+                    aprobados['apellido1'] = inscrito['apellido1']
+                    aprobados['apellido2'] = inscrito['apellido2']
+                    aprobados['estado'] = estado 
+        
+        datos2['aprobados']['P1'].append(aprobados)
+        with open('aprobados.json','w') as archivo:
+            json.dump(datos2,archivo,indent=4)
+
+pruebaInicial()

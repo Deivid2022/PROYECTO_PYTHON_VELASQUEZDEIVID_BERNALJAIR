b0f9c0f86567f214df60a09bd20f18c3a9161780
Deivid <deividvelasquez122@gmail.com>
2024-02-12 13:53:54 -0500

    Trabajo en clase

--- Trainers.py
+++ Trainers.py
@@ -12,7 +12,11 @@ def validarIden(identidad):
     except ValueError:
         return False
     
-
+def leer_trainer():
+    with open('Trainers.json','r') as archivo:
+        data = json.load(archivo)
+        return data
+            
 
 def Añadir_trainer():
     with open('Trainers.json', 'r') as archivo:
@@ -43,12 +47,48 @@ def Añadir_trainer():
     with open('Trainers.json','w') as Archivo:
         json.dump(data,Archivo,indent=4)
 
-
-
-
-
-
-
-Añadir_trainer()
+def Actualizar_trainer():
+    with open('Trainers.json','r') as archivo:
+        data = json.load(archivo)
+    
+    actualizar = data['Trainers']
+    id_trainer = int(input('Ingresa el numero de identidad del Trainer que deseas actualizar: '))
+    for actualiza in actualizar:
+        if actualiza['identidad'] == id_trainer:
+            actualiza['identidad'] = int(input('Ingresa el número de identidad: '))
+            actualiza['nombre'] = input('Ingresa el nombre del Trainer:')
+            actualiza['apellido1'] = input('Ingresa el primer apellido del Trainer: ')
+            actualiza['apellido2'] = input('Ingresa el segundo apellido del Trainer: ')
+            actualiza['direccion'] = input('Ingresa la direccion del Trainer: ')
+            edad = input('Ingresa la edad del Trainer: ')
+            while not validarIden(edad):
+                print('El número de la edad debe ser un entero positivo.')
+                edad = input('Ingresa la edad del Trainer: ')
+            actualiza['edad'] = int(edad)
+            celular = input('Ingresa el número de celular del Trainer: ')
+            while not validarIden(celular):
+                print('El número de celular debe ser un entero positivo.')
+                celular = input('Ingresa el número de celular: ')
+            actualiza['celular'] = int(celular)
+    with open('Trainers.json','w') as archivo:
+        json.dump(data,archivo,indent=4)
+        
+def Eliminar_trainer():
+    with open('Trainers.json','r') as archivo:
+        data = json.load(archivo)
+    
+    eliminar = data['Trainers']
+    id_trianer = int(input('Ingrese el id del Trainer que desea eliminar: '))
+    for elimina in eliminar:
+        if elimina['identidad'] == id_trianer:
+            eliminar.remove(elimina)
+    
+    with open('Trainers.json','w') as archivo:
+        json.dump(data,archivo,indent=4)
+    
+#print(leer_trainer())
+#Actualizar_trainer()
+#Añadir_trainer()
+#Eliminar_trainer()
 
     
--- inscripcionesCampers.py
+++ inscripcionesCampers.py
@@ -26,51 +26,52 @@ def inscripciones():
     
     with open('InscritosCampers.json','r') as archivo:
         data = json.load(archivo)
+    cantidad = int(input('Cuantos campers quieres inscribir: '))
     
-
-    estado = 'Inscrito'
-    nueva_inscripcion = {}
-    identidad = input('Ingresa el número de identidad del camper: ')
-    while not validarIden(identidad):
-        print('El número de identidad debe ser un entero positivo.')
+    for _ in range(cantidad):
+        estado = 'Inscrito'
+        nueva_inscripcion = {}
         identidad = input('Ingresa el número de identidad del camper: ')
-    nueva_inscripcion['identidad'] = int(identidad)
-    nueva_inscripcion['nombre'] = input('Ingresa el nombre del camper: ')
-    nueva_inscripcion['apellido1'] = input('Ingrese el primer apellido del camper: ')
-    nueva_inscripcion['apellido2'] = input('Ingrese el segundo apellido del camper: ')
-    nueva_inscripcion['direccion'] = input('Ingrese la direccion del camper: ')
-    edad = input('Ingrese la edad del camper: ')
-    while not validarEdad(edad):
-        print('La edad debe estar entre 16 y 28 años.')
+        while not validarIden(identidad):
+            print('El número de identidad debe ser un entero positivo.')
+            identidad = input('Ingresa el número de identidad del camper: ')
+        nueva_inscripcion['identidad'] = int(identidad)
+        nueva_inscripcion['nombre'] = input('Ingresa el nombre del camper: ')
+        nueva_inscripcion['apellido1'] = input('Ingrese el primer apellido del camper: ')
+        nueva_inscripcion['apellido2'] = input('Ingrese el segundo apellido del camper: ')
+        nueva_inscripcion['direccion'] = input('Ingrese la direccion del camper: ')
         edad = input('Ingrese la edad del camper: ')
-    nueva_inscripcion['edad'] = int(edad)
-    if nueva_inscripcion['edad'] <= 17:
-        nueva_inscripcion['acudiente'] = input('Ingrese el nombre del acudiente del camper: ')
-        celular_acudiente = input('Ingrese el número de celular del acudiente: ')
-        while not validarIden(celular_acudiente):
-            print('El número de celular del acudiente debe ser un entero positivo.')
+        while not validarEdad(edad):
+            print('La edad debe estar entre 16 y 28 años.')
+            edad = input('Ingrese la edad del camper: ')
+        nueva_inscripcion['edad'] = int(edad)
+        if nueva_inscripcion['edad'] <= 17:
+            nueva_inscripcion['acudiente'] = input('Ingrese el nombre del acudiente del camper: ')
             celular_acudiente = input('Ingrese el número de celular del acudiente: ')
-        nueva_inscripcion['celular_acudiente'] = int(celular_acudiente)
-    else:
-        nueva_inscripcion['acudiente'] = None
-        nueva_inscripcion['celular_acudiente'] = None
+            while not validarIden(celular_acudiente):
+                print('El número de celular del acudiente debe ser un entero positivo.')
+                celular_acudiente = input('Ingrese el número de celular del acudiente: ')
+            nueva_inscripcion['celular_acudiente'] = int(celular_acudiente)
+        else:
+            nueva_inscripcion['acudiente'] = None
+            nueva_inscripcion['celular_acudiente'] = None
 
-    celular = input('Ingresa el número de celular del camper: ')
-    while not validarIden(celular):
-        print('El número de celular debe ser un entero positivo.')
-        celular = input('Ingresa el número de celular: ')
-    nueva_inscripcion['celular'] = int(celular)
-    telefono = input('Ingrese el número fijo: ')
-    while not validarIden(telefono):
-        print('El número fijo debe ser un entero positivo.')
+        celular = input('Ingresa el número de celular del camper: ')
+        while not validarIden(celular):
+            print('El número de celular debe ser un entero positivo.')
+            celular = input('Ingresa el número de celular: ')
+        nueva_inscripcion['celular'] = int(celular)
         telefono = input('Ingrese el número fijo: ')
-    nueva_inscripcion['telefono'] = int(telefono)
-    nueva_inscripcion['estado'] = estado
-    
-    
-    data['datos']['Inscripcion'].append(nueva_inscripcion)
-    with open('InscritosCampers.json','w') as archivo:
-        json.dump(data,archivo,indent=4)
+        while not validarIden(telefono):
+            print('El número fijo debe ser un entero positivo.')
+            telefono = input('Ingrese el número fijo: ')
+        nueva_inscripcion['telefono'] = int(telefono)
+        nueva_inscripcion['estado'] = estado
+        
+        
+        data['datos']['Inscripcion'].append(nueva_inscripcion)
+        with open('InscritosCampers.json','w') as archivo:
+            json.dump(data,archivo,indent=4)
       
 inscripciones()
 

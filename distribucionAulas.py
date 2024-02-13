f2f28e713188fcd14e86a9b35b113861f12c7728
Deivid <deividvelasquez122@gmail.com>
2024-02-12 19:20:08 -0500

    Trabajo en clase

--- distribucionAulas.py
+++ distribucionAulas.py
@@ -1,37 +1,93 @@
-import json
+
 
 def distribucionAulas():
+    import json
     with open('aprobadosYreprobados.json', 'r') as archivo:
         data_aprobados = json.load(archivo)
     with open('aulas.json', 'r') as archivo:
         data_aulas = json.load(archivo)
 
     aprobados = data_aprobados['inscritos']['aprobados']
-    ids_utilizados = set()
+    asignados = data_aulas['aulas']['Aula1']['P1']
+    ids_utilizados = [asignado['identidad'] for asignado in asignados]
+    asignados1 = data_aulas['aulas']['Aula1']['P2']
+    ids_utilizados1 = [asignado['identidad'] for asignado in asignados1]
+    asignados2 = data_aulas['aulas']['Aula1']['P3']
+    ids_utilizados2 = [asignado['identidad'] for asignado in asignados2]
+    asignados3 = data_aulas['aulas']['Aula1']['P4']
+    ids_utilizados3 = [asignado['identidad'] for asignado in asignados3]
+    asignados4 = data_aulas['aulas']['Aula2']['M1']
+    ids_utilizados4 = [asignado['identidad'] for asignado in asignados4]
+    asignados5 = data_aulas['aulas']['Aula2']['M2']
+    ids_utilizados5 = [asignado['identidad'] for asignado in asignados5]
+    asignados6 = data_aulas['aulas']['Aula2']['M3']
+    ids_utilizados6 = [asignado['identidad'] for asignado in asignados6]
+    asignados7 = data_aulas['aulas']['Aula2']['M4']
+    ids_utilizados7 = [asignado['identidad'] for asignado in asignados7]
+    asignados8 = data_aulas['aulas']['Aula3']['J1']
+    ids_utilizados8 = [asignado['identidad'] for asignado in asignados8]
+    asignados9 = data_aulas['aulas']['Aula3']['J2']
+    ids_utilizados9 = [asignado['identidad'] for asignado in asignados9]
+    asignados10 = data_aulas['aulas']['Aula3']['J3']
+    ids_utilizados10 = [asignado['identidad'] for asignado in asignados10]
+    asignados11 = data_aulas['aulas']['Aula3']['J4']
+    ids_utilizados11 = [asignado['identidad'] for asignado in asignados11]
+    
+    id_camper = int(input('Ingresa el número de identidad del camper: '))
+    
+    if id_camper in ids_utilizados:
+        print('Este ID ya ha sido registrado. Por favor ingrese otro.')
+        return
+    if id_camper in ids_utilizados1:
+        print('Este ID ya ha sido registrado. Por favor ingrese otro.')
+        return
+    if id_camper in ids_utilizados2:
+        print('Este ID ya ha sido registrado. Por favor ingrese otro.')
+        return
+    if id_camper in ids_utilizados3:
+        print('Este ID ya ha sido registrado. Por favor ingrese otro.')
+        return
+    if id_camper in ids_utilizados4:
+        print('Este ID ya ha sido registrado. Por favor ingrese otro.')
+        return
+    if id_camper in ids_utilizados5:
+        print('Este ID ya ha sido registrado. Por favor ingrese otro.')
+        return
+    if id_camper in ids_utilizados6:
+        print('Este ID ya ha sido registrado. Por favor ingrese otro.')
+        return
+    if id_camper in ids_utilizados7:
+        print('Este ID ya ha sido registrado. Por favor ingrese otro.')
+        return
+    if id_camper in ids_utilizados8:
+        print('Este ID ya ha sido registrado. Por favor ingrese otro.')
+        return
+    if id_camper in ids_utilizados9:
+        print('Este ID ya ha sido registrado. Por favor ingrese otro.')
+        return
+    if id_camper in ids_utilizados10:
+        print('Este ID ya ha sido registrado. Por favor ingrese otro.')
+        return
+    if id_camper in ids_utilizados11:
+        print('Este ID ya ha sido registrado. Por favor ingrese otro.')
+        return
+           
 
-    while aprobados:
-        id_camper = int(input('Ingresa el número de identidad del camper: '))
-        if id_camper in ids_utilizados:
-            print('Este ID ya ha sido registrado. Por favor ingrese otro')
-            continue
+    Aula = input('Ingresa el aula donde se va establecer el camper(Aula1, Aula2, Aula3): ').capitalize()
+    if Aula == 'Aula1':
+        print('Grupos: P1, P2, P3, P4')
+    elif Aula == 'Aula2':
+        print('Grupos: M1, M2, M3, M4')
+    elif Aula == 'Aula3':
+        print('Grupos: J1, J2, J3, J4')
 
-        ids_utilizados.add(id_camper)
-        Aula = input('Ingresa el aula donde se va establecer el camper(Aula1,Aula2,Aula3): ').capitalize()
-        if Aula == 'Aula1':
-            print('Grupos: P1, P2, P3, P4')
-        elif Aula == 'Aula2':
-            print('Grupos: M1, M2, M3, M4')
-        elif Aula == 'Aula3':
-            print('Grupos: J1, J2, J3, J4')
-            
-        salon = input('Ingrese el grupo en donde quieres asignar el camper: ').upper()
-        
-        capacidad = 33
-        
-        if capacidad <= len(data_aulas['aulas'][Aula][salon]):
-            print('Salón lleno. No se puede asignar más estudiantes.')
-            continue
+    salon = input('Ingrese el grupo en donde quieres asignar el camper: ').upper()
 
+    capacidad = 33
+
+    if capacidad <= len(data_aulas['aulas'][Aula][salon]):
+        print('Salón lleno. No se puede asignar más estudiantes.')
+    else:
         for aprobado in aprobados:
             if aprobado['identidad'] == id_camper:
                 asignar = {
@@ -45,8 +101,6 @@ def distribucionAulas():
                 data_aulas['aulas'][Aula][salon].append(asignar)
                 break
 
-        # Remover al estudiante de la lista de aprobados
-        aprobados = [aprobado for aprobado in aprobados if aprobado['identidad'] != id_camper]
 
         with open('aulas.json', 'w') as archivo:
             json.dump(data_aulas, archivo, indent=4)
--- pruebaInicialCampers.py
+++ pruebaInicialCampers.py
@@ -1,56 +1,51 @@
+
 def pruebaInicial():
-    import json 
+    import json
     
     with open('InscritosCampers.json', 'r') as archivo:
         datos = json.load(archivo)
+        
     with open('aprobadosYreprobados.json', 'r') as archivo:
         datos2 = json.load(archivo)
     
     inscritos = datos['datos']['Inscripcion']  
+    aprobados = datos2['inscritos']['aprobados'] 
+    reprobados = datos2['inscritos']['reprobados']
     
-    ids_utilizados = set()  
     
-    # Continuar el bucle mientras haya inscritos por procesar
-    while inscritos:
+    id_camper = int(input('Ingresa el numero de identidad del camper: '))
         
-        id_camper = int(input('Ingresa el numero de identidad del camper: '))
+    ids_utilizados = [aprobad['identidad'] for aprobad in aprobados]
+    ids_utilizados2 = [reprobado['identidad'] for reprobado in reprobados]
+
+    if id_camper in ids_utilizados:
+        print("Este ID ya ha sido registrado. Por favor, ingresa un ID diferente.")
         
-     
-        if id_camper in ids_utilizados:
-            print("Este ID ya ha sido registrado. Por favor, ingresa un ID diferente.")
-            continue  
-        
-        ids_utilizados.add(id_camper)  
-        
-        aprobados = {}
-        estado = 'Aprobado'
-        estadoR = 'Reprobado'
-        
-        for inscrito in inscritos:
-            if inscrito['identidad'] == id_camper:
-                notaPractica = int(input('Ingrese la nota de la prueba practica: '))
-                notaTeorica = int(input('Ingrese la nota de la prueba teorica: '))
-                notafinal = (notaPractica + notaTeorica)/2
-                if notafinal >= 60:
-                    aprobados['identidad'] = inscrito['identidad']
-                    aprobados['nombre'] = inscrito['nombre']
-                    aprobados['apellido1'] = inscrito['apellido1']
-                    aprobados['apellido2'] = inscrito['apellido2']
-                    aprobados['estado'] = estado 
-                    datos2['inscritos']['aprobados'].append(aprobados)
-                else:
-                    aprobados['identidad'] = inscrito['identidad']
-                    aprobados['nombre'] = inscrito['nombre']
-                    aprobados['apellido1'] = inscrito['apellido1']
-                    aprobados['apellido2'] = inscrito['apellido2']
-                    aprobados['estado'] = estadoR
-                    datos2['inscritos']['reprobados'].append(aprobados)
+    if id_camper in ids_utilizados2:
+        print("Este ID ya ha sido registrado. Por favor, ingresa un ID diferente.")
         
         
-        # Eliminar el inscrito actual de la lista
-        inscritos = [inscrito for inscrito in inscritos if inscrito['identidad'] != id_camper]
-        
-        with open('aprobadosYreprobados.json', 'w') as archivo:
-            json.dump(datos2, archivo, indent=4)  
+    estado = 'Aprobado'
+    estadoR = 'Reprobado'
+    
+    for inscrito in inscritos:
+        if inscrito['identidad'] == id_camper:
+            notaPractica = int(input('Ingrese la nota de la prueba practica: '))
+            notaTeorica = int(input('Ingrese la nota de la prueba teorica: '))
+            notafinal = (notaPractica + notaTeorica)/2
+            nuevo_estudiante = {
+                'identidad': inscrito['identidad'],
+                'nombre': inscrito['nombre'],
+                'apellido1': inscrito['apellido1'],
+                'apellido2': inscrito['apellido2'],
+                'estado': estado if notafinal >= 60 else estadoR
+            }
+            if nuevo_estudiante['estado'] == 'Aprobado':
+                datos2['inscritos']['aprobados'].append(nuevo_estudiante)
+            else:
+                datos2['inscritos']['reprobados'].append(nuevo_estudiante)
+
+    with open('aprobadosYreprobados.json', 'w') as archivo:
+        json.dump(datos2, archivo, indent=4)  
 
 pruebaInicial()

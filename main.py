0b170286a2536bc564549072ec3c03d5b911de17
JairGarcia <156432366+JairBGarcia@users.noreply.github.com>
2024-02-14 21:09:32 -0500

    trabajo en clase

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import os
+import inscripcionesCampers
+import coordinadores
+import distribucionAulas
+import ModuloReportes
+import notas
+import notas2
+import pruebaInicialCampers
+import rutas
+import Trainers
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def get_valid_input(options):
     while True:
@@ -11,8 +24,9 @@ def menu_campers():
     print("Estás en el menú de Campers.")
 
 def menu_inscripcionesCampers():
+    clear_console()
     while True:
-        print("\nMenu de Inscripciones de Campers:")
+        print("Menu de Inscripciones de Campers:")
         print("1. Inscripciones")
         print("2. Actualizar Campers")
         print("3. Buscar")
@@ -22,65 +36,131 @@ def menu_inscripcionesCampers():
         user_input = get_valid_input(["1", "2", "3", "4", "5"])
         if user_input == "1":
             print("Accediste a Inscripciones de Campers.")
-
+            inscripcionesCampers.inscripciones()
         elif user_input == "2":
             print("Accediste a Actualizar Campers.")
+            inscripcionesCampers.actualizar_campers()
         elif user_input == "3":
             print("Accediste a Buscar.")
+            inscripcionesCampers.buscar()
         elif user_input == "4":
             print("Accediste a Mostrar Datos.")
+            inscripcionesCampers.mostrar_datos()
         elif user_input == "5":
             print("Volviendo al menú principal.")
             break
 
-def menu_trainers():
-    print("Estás en el menú de Trainers.")
+def leer_trainers():
+    print("Accediste a Leer Trainers.")
 
-def menu_coordinadores():
+def añadir_trainers():
+    print("Accediste a Añadir Trainers.")
+
+def actualizar_trainers():
+    print("Accediste a Actualizar Trainers.")
+
+def eliminar_trainers():
+    print("Accediste a Eliminar Trainers.")
+
+def menu_trainers():
+    clear_console()
     while True:
-        print("\nMenu de Coordinadores:")
-        print("1. Agregar Coordinadores")
-        print("2. Prueba Inicial")
-        print("3. Distribución de Aulas")
-        print("4. Módulo de Reportes")
+        print("Menu de Trainers:")
+        print("1. Leer Trainers")
+        print("2. Añadir Trainers")
+        print("3. Actualizar Trainers")
+        print("4. Eliminar Trainers")
         print("5. Volver al menú principal")
 
         user_input = get_valid_input(["1", "2", "3", "4", "5"])
         if user_input == "1":
-            print("Accediste a Agregar Coordinadores.")
+            Trainers.leer_trainer()
         elif user_input == "2":
-            print("Accediste a Prueba Inicial.")
+            Trainers.Añadir_trainer()
         elif user_input == "3":
-            print("Accediste a Distribución de Aulas.")
+            Trainers.Actualizar_trainer()
         elif user_input == "4":
-            menu_reportes()
+            Trainers.Eliminar_trainer()
         elif user_input == "5":
             print("Volviendo al menú principal.")
             break
 
-def menu_reportes():
+def menu_coordinadores():
+    clear_console()
     while True:
-        print("\nMenu de Reportes:")
+        print("Menu de Coordinadores:")
+        print("1. Agregar Coordinadores")
+        print("2. Prueba Inicial")
+        print("3. Distribución de Aulas")
+        print("4. Módulo de Reportes")
+        print("5. Notas")
+        print("6. Volver al menú principal")
+
+        user_input = get_valid_input(["1", "2", "3", "4", "5", "6"])
+        if user_input == "1":
+            print("Accediste a Agregar Coordinadores.")
+            coordinadores.Coordinadores()
+        elif user_input == "2":
+            print("Accediste a Prueba Inicial.")
+            pruebaInicialCampers.pruebaInicial()
+        elif user_input == "3":
+            print("Accediste a Distribución de Aulas.")
+            distribucionAulas.distribucionAulas()
+        elif user_input == "4":
+            menu_reportes()
+        elif user_input == "5":
+            menu_notas()
+        elif user_input == "6":
+            print("Volviendo al menú principal.")
+            break
+
+def menu_reportes():
+    clear_console()
+    while True:
+        print("Menu de Reportes:")
         print("1. Camper Estado Inscrito")
         print("2. Camper Estado Aprobado")
         print("3. Trainers")
         print("4. Bajo Rendimiento")
-        print("5. Volver al menú de Coordinadores")
+        print("5. Camper Función")
 
         user_input = get_valid_input(["1", "2", "3", "4", "5"])
         if user_input == "1":
             print("Accediste a Camper Estado Inscrito.")
+            ModuloReportes.camperEstadoInscrito()
         elif user_input == "2":
             print("Accediste a Camper Estado Aprobado.")
+            ModuloReportes.camperEstadoAprobado()
         elif user_input == "3":
             print("Accediste a Trainers.")
+            ModuloReportes.Trainers()
         elif user_input == "4":
             print("Accediste a Bajo Rendimiento.")
+            ModuloReportes.bajoRendimiento()
         elif user_input == "5":
+            print("Accediste a Camper Función.")
+            ModuloReportes.CamperFuncion()
+
+def menu_notas():
+    clear_console()
+    while True:
+        print("Menu de Notas:")
+        print("1. Actualizar Nota y Rendimiento")
+        print("2. Volver al menú de Coordinadores")
+
+        user_input = get_valid_input(["1", "2", "3", "4", "5", "6"])
+        if user_input == "1":
+            notas.actualizar_notas_y_rendimiento()
+        elif user_input == "2":
             print("Volviendo al menú de Coordinadores.")
             break
 
+def actualizar_nota_y_rendimiento():
+    print("Accediste a Actualizar Nota y Rendimiento.")
+
+
 def menu_principal():
+    clear_console()
     while True:
         print("Bienvenido al sistema de gestión:")
         print("1. Inscripciones de Campers")
